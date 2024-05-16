9338e478671058644f9aeadae3a3306781fb8a36
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2024-05-16 22:57:23 +0200

    create function with menu list object

---
+++ src/js/menuList.js
@@ -0,0 +1,59 @@
+export default function menuList() {
+  
+  const menu = {
+        "antipasti": [
+          {
+            "nome": "Bruschetta al Pomodoro",
+            "prezzo": "5.00",
+            "ingredienti": ["pane casereccio", "pomodori", "aglio", "basilico", "olio di oliva"]
+          },
+          {
+            "nome": "Caprese",
+            "prezzo": "6.50",
+            "ingredienti": ["mozzarella di bufala", "pomodori", "basilico", "olio di oliva"]
+          },
+          {
+            "nome": "Antipasto all'Italiana",
+            "prezzo": "9.00",
+            "ingredienti": ["salumi assortiti", "formaggi misti", "olive", "carciofini"]
+          }
+        ],
+        "pizze": [
+          {
+            "nome": "Margherita",
+            "prezzo": "7.00",
+            "ingredienti": ["pomodoro", "mozzarella", "basilico"]
+          },
+          {
+            "nome": "Diavola",
+            "prezzo": "8.50",
+            "ingredienti": ["pomodoro", "mozzarella", "salame piccante"]
+          },
+          {
+            "nome": "Quattro Stagioni",
+            "prezzo": "9.00",
+            "ingredienti": ["pomodoro", "mozzarella", "funghi", "carciofi", "olive", "prosciutto cotto"]
+          }
+        ],
+        "dessert": [
+          {
+            "nome": "Tiramisù",
+            "prezzo": "5.00",
+            "ingredienti": ["mascarpone", "caffè", "savoiardi", "cacao"]
+          },
+          {
+            "nome": "Panna Cotta",
+            "prezzo": "4.50",
+            "ingredienti": ["panna", "vaniglia", "gelatina", "salsa di frutti di bosco"]
+          },
+          {
+            "nome": "Gelato Artigianale",
+            "prezzo": "3.50",
+            "varianti": ["cioccolato", "vaniglia", "fragola"]
+          }
+        ]
+      }
+
+      return menu
+    };
+  
