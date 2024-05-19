fb4a1cc0855014b2c82f0d425606f553a4163a43
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2024-05-19 16:13:41 +0200

    refactor: Change to use initializeButtonListeners for better encapsulation

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,14 @@
 import '../css/styles.css'
 
 import addHomeElement from './home.js'
-import handleButtonClick from './navInteraction.js'
+import initializeButtonnLister from './navInteraction.js'
 
-const navbar = document.querySelector('.nav-bar') 
 
 addHomeElement()
 // addAboutElement()
 // addMenuElement()
-navbar.addEventListener('click', handleButtonClick)
 
-
-
+initializeButtonnLister()
 
 
 
