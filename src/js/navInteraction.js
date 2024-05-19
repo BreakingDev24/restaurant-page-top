72a812f817b05bb24fe09465422ac6f4187f8e1b
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2024-05-19 16:09:05 +0200

    refactor: Create closure to encapsulate button click event handling and state

--- src/js/navInteraction.js
+++ src/js/navInteraction.js
@@ -5,35 +5,39 @@ import addMenuElement from './menu.js'
 
 
 console.log("Hello World!");
+const navbar = document.querySelector('.nav-bar') 
 
 
-let lastClickedButton = null
-export default function handleButtonClick(e){
-    let eventTarget = e.target;
-
-    if(lastClickedButton){
-        lastClickedButton.classList.remove('active')
-    }
-
-    eventTarget.classList.add('active')
-    lastClickedButton = eventTarget
-
-    const content = document.getElementById('content');
-    content.innerHTML = '';
+export default function initializeButtonnLister(){
+    let lastClickedButton = null
+    function handleButtonClick(e){
+        let eventTarget = e.target;
     
-    switch(eventTarget.id){
-        case 'home-btn':
-            addHomeElement();
-            break;
-        case 'menu-btn':
-            addMenuElement();
-            break;
-        case 'about-btn':
-            addAboutElement();
-            break
+        if(lastClickedButton){
+            lastClickedButton.classList.remove('active')
+        }
+    
+        eventTarget.classList.add('active')
+        lastClickedButton = eventTarget
+    
+        const content = document.getElementById('content');
+        content.innerHTML = '';
+        
+        switch(eventTarget.id){
+            case 'home-btn':
+                addHomeElement();
+                break;
+            case 'menu-btn':
+                addMenuElement();
+                break;
+            case 'about-btn':
+                addAboutElement();
+                break
+        }
     }
+    navbar.addEventListener('click', handleButtonClick)
+    
 }
-
 
 
 
