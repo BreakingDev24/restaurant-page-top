65021b9380432f78106a7ad339821ac494699ceb
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2024-06-01 15:25:45 +0200

    import addDateFooter

--- src/js/index.js
+++ src/js/index.js
@@ -2,10 +2,10 @@ import '../css/styles.css'
 
 import addHomeElement from './home.js'
 import initializeButtonnLister from './navInteraction.js'
-
+import { addDateFooter } from './dateUtils.js'
 
 addHomeElement()
-
+addDateFooter()
 initializeButtonnLister()
 
 
